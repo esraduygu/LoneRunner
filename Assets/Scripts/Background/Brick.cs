a901bed5cbac46b0d8e7e53b55cc3a0ce726eae4
Esra Duygu <benesrakaya@gmail.com>
2024-02-28 22:02:09 +0300

    Implemented platform movement.

---
+++ Assets/Scripts/Background/Brick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Background
+{
+    public class Brick : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        
+        private Vector3 _internalPosition;
+
+        private void Awake()
+        {
+            _internalPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            _internalPosition += Vector3.left * (speed * Time.deltaTime);
+            
+            if (_internalPosition.x < -320)
+                _internalPosition.x = 320;
+
+            transform.position = Vector3Int.FloorToInt(_internalPosition);
+        }
+    }
+}
