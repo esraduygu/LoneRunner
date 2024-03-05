f8287968cd9e9a794d792fdfdf871d3dc1119f81
Esra Duygu <benesrakaya@gmail.com>
2024-03-05 20:33:55 +0300

    Cleanup.

--- Assets/Scripts/Obstacles/ObstacleSpawner.cs
+++ Assets/Scripts/Obstacles/ObstacleSpawner.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using Utilities;
 
 namespace Obstacles
 {
@@ -20,28 +20,9 @@ namespace Obstacles
             const float normalProbability = 0.4f;
             const float hardProbability = 0.1f;
             
-            var index = PickOne(easyProbability, normalProbability, hardProbability);
+            var index = MathUtilities.PickOne(easyProbability, normalProbability, hardProbability);
 
             Instantiate(obstaclePrefabs[index], transform.position, quaternion.identity);
-        }
-        
-        public static int PickOne(params float[] probabilities)
-        {
-            var probabilityIndices = probabilities
-                .Select((value, index) => new { value, index })
-                .ToDictionary(p => p.index, p => p.value)
-                .OrderBy(x => x.Value);
-
-            var random = Random.value;
-            foreach (var (index, probability) in probabilityIndices)
-            {
-                random -= probability;
-
-                if (random < 0)
-                    return index;
-            }
-
-            return -1;
         }
     }
 }
---
+++ Assets/Scripts/Utilities/MathUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class MathUtilities
+    {
+        public static int PickOne(params float[] probabilities)
+        {
+            var probabilityIndices = probabilities
+                .Select((value, index) => new { value, index })
+                .ToDictionary(p => p.index, p => p.value)
+                .OrderBy(x => x.Value);
+
+            var random = Random.value;
+            foreach (var (index, probability) in probabilityIndices)
+            {
+                random -= probability;
+
+                if (random < 0)
+                    return index;
+            }
+
+            return -1;
+        }
+    }
+}
