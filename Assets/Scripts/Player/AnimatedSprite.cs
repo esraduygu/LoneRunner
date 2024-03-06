54db8db71b4166d12c0f12ba29a65e69f2c57917
Esra Duygu <benesrakaya@gmail.com>
2024-03-06 13:41:34 +0300

    Animation for the player.

---
+++ Assets/Scripts/Player/AnimatedSprite.cs
@@ -0,0 +1,34 @@
+﻿using Core;
+using UnityEngine;
+
+namespace Player
+{
+    public class AnimatedSprite : MonoBehaviour
+    {
+        [SerializeField] private GameManager gameManager;
+        [SerializeField] private Sprite[] sprites;
+        [SerializeField] private float baseFps;
+        
+        private SpriteRenderer _spriteRenderer;
+        private int _frame;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (gameManager.State == GameManager.GameState.Playing)
+                Animate();
+                
+        }
+
+        private void Animate()
+        {
+            var fps = baseFps * gameManager.gameSpeed;
+            var spriteIndex = Mathf.FloorToInt(Time.time % 1 * fps) % sprites.Length;
+            _spriteRenderer.sprite = sprites[spriteIndex];
+        }
+    }
+}
