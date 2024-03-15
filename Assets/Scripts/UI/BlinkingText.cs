f5aa8cef5dddb3698505475ffc1a285f5935a89e
Esra Duygu <benesrakaya@gmail.com>
2024-03-15 19:24:22 +0300

    Refactoring.

--- Assets/Scripts/UI/BlinkingText.cs
+++ Assets/Scripts/UI/BlinkingText.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.Audio;
 
 namespace UI
 {
     public class BlinkingText : MonoBehaviour
     {
-        [Range(1, 10)]
-        public float blinkingRate;
-        
+        [Range(1, 10)] public float blinkingRate;
+
         [SerializeField] private TMP_Text text;
-        [SerializeField] private AudioSource audioSource;
-        [SerializeField] private AudioMixer mixer;
 
         private void Update()
         {
-            // text.alpha = Mathf.Sin(Time.time * blinkingRate) > 0 ? 1 : 0;
-            
-            // text.alpha = mixer.
+            text.alpha = Mathf.Sin(Time.time * blinkingRate) >= 0 ? 1 : 0;
         }
     }
 }
