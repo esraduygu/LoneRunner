e9c3949f87cf87bb8127197b7aba2d0f54b66e0a
Esra Duygu <benesrakaya@gmail.com>
2024-03-16 13:35:57 +0300

    Added tilemap.

---
+++ Assets/Scripts/Background/GroundGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Core;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Background
+{
+    public class GroundGenerator : MonoBehaviour
+    {
+        [SerializeField] private GameManager gameManager;
+        [SerializeField] private Tilemap groundTileMap;
+        [SerializeField] private Tile groundTile;
+
+        private Vector3Int startingPos = new(10, -2, 0);
+        private float movementSinceLastTile = 16;
+        
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            var moveAmount = gameManager.gameSpeed * Time.deltaTime;
+            transform.position += Vector3.left * moveAmount;
+
+            var ogMoveAmount = moveAmount;
+            var i = 0;
+
+            movementSinceLastTile += moveAmount;
+            while (movementSinceLastTile > 16)
+            {
+                i++;
+                groundTileMap.SetTile(startingPos, groundTile);
+                startingPos.x++;
+                movementSinceLastTile -= 16;
+            }
+            
+            Debug.Log($"Spawned {i} tiles this frame. Moved {ogMoveAmount} pixels?");
+        }
+    }
+}
